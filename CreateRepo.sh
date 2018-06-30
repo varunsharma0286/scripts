3f76bc9ef63f4fb9e47391fdd7073d1b2c8ac2d8
varunsharma0286 <vsharma7@buffalo.edu>
2018-06-29 23:58:32 -0400

    Removing logs

--- CreateRepo.sh
+++ CreateRepo.sh
@@ -6,15 +6,8 @@ if [ "$#" -ne 2 ]; then
 	exit
 fi
 
-echo '$1'
-echo "$2"
 USERNAME=$1
 REPO=$2
-
-echo $USERNAME
-echo $REPO
-
-
 curl -u $USERNAME https://api.github.com/user/repos -d "{\"name\":\"$REPO\"}"
 #curl -u ${USER:-${GITHUBUSER}} https://api.github.com/user/repos -d "{\"name\": \"${REPONAME:-${CURRENTDIR}}\", \"description\": \"${DESCRIPTION}\", \"private\": false, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
 #git remote set-url origin git@github.com:$$USERNAME/$$REPO.git
